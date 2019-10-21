e94e06bcda3d717fe06ed2d95088089803609c8a
Ronak Shah <rons.shah@gmail.com>
2019-10-21 19:44:17 -0400

    Update test_merge_fastq.py

--- tests/test_merge_fastq.py
+++ tests/test_merge_fastq.py
@@ -3,8 +3,6 @@
 
 """Tests for `merge_fastq` package."""
 
-from process_fastq import cli
-import unittest
 import pytest
 
 from click.testing import CliRunner
@@ -34,7 +32,7 @@ def test_command_line_interface():
     runner = CliRunner()
     #result = runner.invoke(cli.main)
     #assert result.exit_code == 0
-    #assert 'process_fastq.cli.main' in result.output
+    #assert 'merge_fastq.cli.main' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help                          Show this message and exit.' in help_result.output
