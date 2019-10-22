45a51dbff66d5893623fb6ee304582457efc0092
Ronak Shah <rons.shah@gmail.com>
2019-10-22 16:50:53 -0400

    Update merge_fastq.py

--- merge_fastq/merge_fastq.py
+++ merge_fastq/merge_fastq.py
@@ -38,7 +38,8 @@ def run(fastq1, fastq2, output_path, out_fastq1, out_fastq2):
                 exit(1)
         else:
             merge_fastq(fastq1, fastq2, out_file1, out_file2)
-            logging.info("Done merging fastq file in %s and %s", out_file1, out_file2)
+            logging.info("Done merging fastq file in %s and %s",
+                         out_file1, out_file2)
 
     else:
         logger.error("The program expects that the same number of fastq are provided for READ1 and READ2, current they dont match. \n\n ### READ1 ### \n %s \n ### READ2 ### \n %s \n", fastq1, fastq2)
@@ -46,11 +47,14 @@ def run(fastq1, fastq2, output_path, out_fastq1, out_fastq2):
 
 
 def merge_fastq(fastq_list_R1, fastq_list_R2, out_file1, out_file2):
-    with open(out_file1, 'wb') as outfile:
-        for fastq in fastq_list_R1:
-            with open(fastq, 'rb') as infile:
-                shutil.copyfileobj(infile, outfile)
-    with open(out_file2, 'wb') as outfile:
-        for fastq in fastq_list_R2:
-            with open(fastq, 'rb') as infile:
-                shutil.copyfileobj(infile, outfile)
+    # READ1
+    with open(out_file1, 'wb') as outfile1:
+        for fq1 in fastq_list_R1:
+            with open(fq1, 'rb') as infile1:
+                shutil.copyfileobj(infile1, outfile1)
+    # READ2
+    with open(out_file2, 'wb') as outfile2:
+        for fq2 in fastq_list_R2:
+            with open(fq2, 'rb') as infile2:
+                shutil.copyfileobj(infile2, outfile2)
+    return
